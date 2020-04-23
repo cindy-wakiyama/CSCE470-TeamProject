3f766bca33ec0deb2145fc364f54a3fd7627175b
Faheem Noorani <faheemnoorani@Faheems-MacBook-Air.local>
2020-04-23 12:51:07 -0500

    Updated readme and added playlist to results

--- GetSentiment.js
+++ GetSentiment.js
@@ -14,7 +14,7 @@ function train(){
                 key += sepVocab[i][j];
             }
             else if (sepVocab[i][j] != '\t'){
-                value += sepVocab[i][j]
+                value += sepVocab[i][j];
             }
             if(sepVocab[i][j] == '\t'){
                 found = true;
@@ -31,22 +31,30 @@ var vocabDictionary = train();
 var analyzeText = readlineSync.question('Text to Analyze: ');
 console.log('Analyzing {' + analyzeText + '}');
 var words = analyzeText.split(/\W/);
-var score = 0
+var score = 0;
 for(var i = 0; i < words.length; i++){
     var word = words[i].toLocaleLowerCase();
     if(vocabDictionary.hasOwnProperty(word)){
-        score += vocabDictionary[word]
+        score += vocabDictionary[word];
     }
 }
 
 /* Compute Score and Return Analysis */
-console.log("Score: " + score)
-console.log("Comparative Score: " + score / words.length)
+console.log("Score: " + score);
+console.log("Comparative Score: " + score / words.length);
 if(score / words.length > 0){
-    console.log('Positive')
+    console.log('Positive');
+    console.log("Playlists to keep you feeling positive!");
+    console.log("https://open.spotify.com/playlist/37i9dQZF1DXdPec7aLTmlC");
+    console.log("https://open.spotify.com/playlist/4FYU1aJih7ykRIUYenRt8w");
 }else if(score / word.length < 0){
-    console.log('Negative')
+    console.log('Negative');
+    console.log('Playlists to help you feel better!');
+    console.log("https://open.spotify.com/playlist/37i9dQZF1DX3rxVfibe1L0");
+    console.log("https://open.spotify.com/playlist/37i9dQZF1DWTwnEm1IYyoj");
+    console.log("https://open.spotify.com/playlist/37i9dQZF1DX889U0CL85jj");
 }else{
-    console.log('Neutral')
+    console.log('Neutral');
+    console.log("https://open.spotify.com/playlist/37i9dQZF1DWTwnEm1IYyoj");
+    console.log("https://open.spotify.com/playlist/37i9dQZF1DX889U0CL85jj");
 }
-
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # CSCE470-TeamProject
+To run this project you will need a few things installed:
+    node.js (version 12.16.2)
+    readline-sync repo (npm i readline-sync)
+
+To start the project, in terminal, navigate to the root folder and run <node GetSentiment.js>
+Now enter a tweet or any text corpus you want to analyze into the terminal as the prompt asks.
+Once you entered the text, hit enter and the result will show its score, comparative score, and its overall mood assessment (positive, neutral, or negative).
